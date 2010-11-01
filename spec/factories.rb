c1a486e78180120ad81435f7ac5843c7631b3c74
danstorms <dan@redroverhq.com>
2010-11-01 17:08:19 -0400

    Fixed sign in

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,8 +1,8 @@
 
 # Simulate the User model with Factory Girl
 Factory.define :user do |user|
-  user.name                  "Dan Storms"
-  user.email                 "dan@example.com"
+  user.name                  "Stupid Storms"
+  user.email                 "stupid@example.com"
   user.password              "foobar"
   user.password_confirmation "foobar"
 end
